885317a4bf68c8b30579c60c96da41336d16fa7a
Woody Maddox <woody.e.maddox@gmail.com>
2019-04-22 19:44:54 -0500

    aded buttons

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -4,12 +4,33 @@ import './locations.scss';
 
 let locations = [];
 
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-secondary';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-success';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-info';
+      break;
+    case 'After Dark':
+      selectedClass = 'bg-danger';
+      break;
+    default:
+      selectedClass = '';
+  }
+  return selectedClass;
+};
+
 const domStringBuilder = () => {
   let domString = '';
   locations.forEach((location) => {
     domString += `<div id=${location.id} class="card location col-2">`;
     domString += '<div class="card">';
-    domString += '<div class="card-header">Card Title';
+    domString += `<h3 class="card-header ${shootTimeClass(location.shootTime)}">${location.name}</h3>`;
     domString += `<h5>${location.name}</h5>`;
     domString += `<h5>${location.address}</h5>`;
     domString += `<div class="image"><img src=${location.imageUrl} style="width:70%"></div>`;
